1050e81d98a334ba19a6e8e905408f950c30eaac
Paulo <Paulo@DESKTOP-Q3GLC8N>
2024-10-03 17:27:16 -0300

    cmt

---
+++ jogo_corrida/src/jogo_corrida/Sound.java
@@ -0,0 +1,38 @@
+package jogo_corrida;
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+
+public class Sound {
+
+	private AudioClip clip; 
+	
+	public static final Sound musicBackground = new Sound("/msc.wav");
+	public static final Sound s = new Sound("/largada.wav");
+	
+	
+	private Sound(String name) {
+		try {
+			clip = Applet.newAudioClip(Sound.class.getResource(name));
+		}catch(Throwable e) {}
+	}
+	
+	public void play() {
+		try {
+			new Thread() {
+				public void run() {
+					clip.play();
+				}
+			}.start();
+		}catch(Throwable e) {}
+	}
+	public void loop() {
+		try {
+			new Thread() {
+				public void run() {
+					clip.loop();
+				}
+			}.start();
+		}catch(Throwable e) {}
+	}
+}
